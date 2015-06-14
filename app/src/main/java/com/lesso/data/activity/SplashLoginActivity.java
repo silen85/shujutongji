1456bcb14221f25acd4e2223b877bf9763b23b6a
silen85 <jqrs008@163.com>
2015-06-14 14:28:51 +0800

    shujutongji

---
+++ app/src/main/java/com/lesso/data/activity/SplashLoginActivity.java
@@ -0,0 +1,57 @@
+package com.lesso.data.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.lesso.data.R;
+import com.lesso.data.fragment.LoginFragment;
+import com.lesso.data.fragment.SplashFragment;
+
+
+public class SplashLoginActivity extends FragmentActivity {
+
+    private FragmentManager fragmentManager;
+
+    private Fragment splashFragment;
+    private Fragment loginFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+
+        fragmentManager =getSupportFragmentManager();
+
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        splashFragment = new SplashFragment();
+        fragmentTransaction.add(R.id.splash_login, splashFragment);
+        fragmentTransaction.commit();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                loginFragment = new LoginFragment();
+
+                fragmentTransaction.remove(splashFragment);
+
+                fragmentTransaction.add(R.id.splash_login, loginFragment);
+                fragmentTransaction.commit();
+
+
+            }
+        }, 6000);
+
+
+
+    }
+
+}
